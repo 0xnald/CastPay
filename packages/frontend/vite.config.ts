cc06602c681f6dcdc1a618752b58c507bb26e909
0xnald <chinaldo.zo@gmail.com>
2026-06-24 10:46:39 +0100

    feat(frontend): automatically load env variables from root .env.local file in Vite config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import * as path from "path";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills({
-      globals: {
-        Buffer: true,
-        global: true,
-        process: true,
-      },
-      protocolImports: true,
-    }),
-  ],
-  define: {
-    "process.env.RPC": JSON.stringify(process.env.RPC || "https://rpc.testnet.arc.network"),
-  },
-  server: {
-    port: 3000,
-    host: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, path.resolve(__dirname, "../../"), "");
+  return {
+    plugins: [
+      react(),
+      nodePolyfills({
+        globals: {
+          Buffer: true,
+          global: true,
+          process: true,
+        },
+        protocolImports: true,
+      }),
+    ],
+    define: {
+      "process.env.RPC": JSON.stringify(process.env.RPC || env.RPC || "https://rpc.testnet.arc.network"),
+    },
+    server: {
+      port: 3000,
+      host: true,
+    },
+  };
 });
 
